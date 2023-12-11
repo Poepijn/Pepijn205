1331fa91666e5a8edf3b667e4e4af318b14d1b19
Poepijn <150830144+Poepijn@users.noreply.github.com>
2023-12-11 22:47:48 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,21 @@
 // JavaScript Document
 console.log("hi");
+
+var openButton = document.querySelector("header > button");
+
+var deNavigatie = document.querySelector("header nav");
+
+var sluitButton = document.querySelector("header nav button")
+
+
+openButton.onclick = openMenu;
+
+sluitButton.onclick = sluitMenu;
+
+function openMenu(){
+deNavigatie.classList.add("toonMenu")
+}
+
+function sluitMenu(){
+    deNavigatie.classList.remove("toonMenu")
+}
